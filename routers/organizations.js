a0a790ca11b3cd943cb9284eb4ad3c096581b968
adammalej <adammalej1@gmail.com>
2023-04-24 17:38:30 +0200

    feat: add uuid package

--- routers/organizations.js
+++ routers/organizations.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server')
 const express = require('express')
 const organizations = jsonServer.router('./databases/organizations.json').db.get('organizations')
 const router = express.Router()
+const { v4: uuidv4 } = require('uuid');
 
 router.post('/', (req, res) => {
   const { name } = req.body
+  const id = uuidv4()
 
   const existingOrganization = organizations.find({ name }).value()
 
@@ -12,7 +14,7 @@ router.post('/', (req, res) => {
     return res.status(400).send({ error: 'ORGANIZATION_ALREADY_EXIST' })
   }
 
-  organizations.push({ name }).write()
+  organizations.push({ id, name }).write()
 
   return res.json({ message: 'Organization added successfully' })
 })
--- routers/projects.js
+++ routers/projects.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server')
 const express = require('express')
 const projects = jsonServer.router('./databases/projects.json').db.get('projects')
 const router = express.Router()
+const { v4: uuidv4 } = require('uuid');
 
 router.post('/', (req, res) => {
   const { name } = req.body
+  const id = uuidv4()
 
   const existingProject = projects.find({ name }).value()
 
@@ -12,9 +14,13 @@ router.post('/', (req, res) => {
     return res.status(400).send({ error: 'PROJECT_ALREADY_EXIST' })
   }
 
-  projects.push({ name }).write()
+  projects.push({ id, name }).write()
 
   return res.json({ message: 'Project added successfully' })
+})
+
+router.get('/', (req, res) => {
+  return res.json(projects)
 })
 
 module.exports = router
